cd1d8a83f81d1bbec32355acbecc0e33301d9a44
sank07 <77034706+sank07@users.noreply.github.com>
2024-08-30 11:52:21 +0000

    chalo aur code lelo

---
+++ basics01/obj.js
@@ -0,0 +1,11 @@
+// object creation
+const myObj={
+    name:"sanket",
+    lname:" pandit",
+    email:"sanket@gmail.com",
+    contact:"1234567895",
+    loggedIN:false,
+    lastLogIn:["mon","sat"]
+}
+
+console.log(myObj.lname)
